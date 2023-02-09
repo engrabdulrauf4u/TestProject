9d1b9b41cc21427d62d94e79b1b1d917aca757e9
engrabdulrauf4u <121174962+engrabdulrauf4u@users.noreply.github.com>
2023-02-09 16:18:34 +0500

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -10,6 +10,6 @@ public class HelloWorldController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World ....Feb 9 04:03 PM";
+        return "Hello World ....Feb 9 04:20 PM";
     }
 }
