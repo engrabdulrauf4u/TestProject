1c92bd9376fceed5bdc7ed58b989ed9c92828187
engrabdulrauf4u <engrabdulrauf4u@gmail.com>
2023-02-10 10:54:38 +0500

    servelt initializer

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,9 +2,11 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloworldApplication {
+//public class HelloworldApplication {
+public class HelloworldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
